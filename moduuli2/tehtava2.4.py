d835ca7fb51973723582744ea7d91420e87d5aa3
RoopeJo <roope.jokinen@.metropolia.fi>
2025-01-16 13:04:28 +0200

    tehtava 2.1

---
+++ moduuli2/tehtava2.4.py
@@ -0,0 +1,7 @@
+Luku1 = float(input("luku?"))
+Luku2 = float(input("luku?"))
+Luku3 = float(input("luku?"))
+
+print(Luku1 + Luku2 + Luku3)
+print(Luku1 * Luku2 * Luku3)
+print(Luku1 * Luku2 * Luku3 / 3)
