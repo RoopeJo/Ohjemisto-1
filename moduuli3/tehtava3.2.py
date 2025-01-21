c1226057675dcf4fdff64a916d6edfcdb33832f5
RoopeJo <roope.jokinen@.metropolia.fi>
2025-01-21 14:34:05 +0200

    3.2

---
+++ moduuli3/tehtava3.2.py
@@ -0,0 +1,16 @@
+Hytti_luokka = input("laivan hyttiluokka?")
+
+if Hytti_luokka == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella")
+
+if Hytti_luokka == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella")
+
+if Hytti_luokka == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella")
+
+if Hytti_luokka == "C":
+    print("C on ikkunaton hytti autokannen alapuolella")
+
+else:
+    print("Virheellinen hyttiluokka")
